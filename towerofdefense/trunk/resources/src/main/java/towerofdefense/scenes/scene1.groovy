6632ffe4c6cc6c9f5868f200fdc4e4eb62c8c2c5
ruben01 <ruben01@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-02-11 21:15:03 +0000

    added hack to deploy turrets
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@21 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

--- towerofdefense/trunk/resources/src/main/java/towerofdefense/scenes/scene1.groovy
+++ towerofdefense/trunk/resources/src/main/java/towerofdefense/scenes/scene1.groovy
@@ -1,5 +1,27 @@
 package towerofdefense.scenes;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Input 
+import org.newdawn.slick.geom.Vector2f;
+
+import com.gemserk.componentsengine.entities.Entity;
+import com.gemserk.componentsengine.messages.AddEntityMessage;
+import com.gemserk.componentsengine.messages.MessageQueue 
+import com.gemserk.componentsengine.templates.EntityTemplate;
+
+import com.gemserk.componentsengine.templates.TemplateProvider;
+
+import com.google.inject.Inject;
+
+import com.google.inject.Inject;
+
+import com.gemserk.componentsengine.messages.GenericMessage;
+
+import com.gemserk.componentsengine.templates.TemplateProvider;
+
+
+
+import com.gemserk.componentsengine.components.ReflectionComponent;
 import com.gemserk.games.towerofdefense.Path;
 
 builder.scene("todh.scenes.scene1") {
@@ -43,6 +65,41 @@ builder.scene("todh.scenes.scene1") {
 				]		
 	}
 	
+	component(new ReflectionComponent("towerDeployer"){
+		
+		@Inject TemplateProvider templateProvider
+		@Inject Input input;
+		@Inject MessageQueue messageQueue;
+		
+		public void handleMessage(GenericMessage message){
+			if(message.id == "deployturret"){
+				def parameters = [
+				                  position:new Vector2f(input.getMouseX(),input.getMouseY()),
+				                  direction:new Vector2f(-1,0),
+				                  radius:100f,
+				                  lineColor:new Color(0.0f, 0.0f, 0.0f, 1.0f),
+				                  fillColor:new Color(0.0f, 0.0f, 0.0f, 0.2f),
+				                  color:new Color(0.0f, 0.2f, 0.0f, 1.0f),
+				                  template:"towerofdefense.entities.bullet",
+				                  reloadTime:1000,
+				                  instanceParameters: [
+				                                       damage:0.0f,
+				                                       radius:10.0f,
+				                                       maxVelocity:0.5f,
+				                                       color:new Color(0.4f, 1.0f, 0.4f, 1.0f)
+				                                       ]		
+				                                       ];
+				
+				 EntityTemplate bulletTemplate = templateProvider.getTemplate("towerofdefense.entities.tower");
+				
+				Entity tower = bulletTemplate.instantiate("tower-${Math.random()}", parameters);
+				
+				messageQueue.enqueue(new AddEntityMessage(tower));
+			}
+		}
+	});
+	
+	
 	entity(template:"towerofdefense.entities.tower", id:"tower1")	{
 		position=utils.vector(350,350)
 		direction=utils.vector(-1,0)
@@ -60,6 +117,15 @@ builder.scene("todh.scenes.scene1") {
 				maxVelocity:0.5f,
 				color:utils.color(0.4f, 1.0f, 0.4f, 1.0f)
 				]		
+	}
+	
+	input("inputmapping"){
+		mouse {
+		
+			press(button:"left", eventId:"deployturret");
+
+		
+		}
 	}
 	
 }
