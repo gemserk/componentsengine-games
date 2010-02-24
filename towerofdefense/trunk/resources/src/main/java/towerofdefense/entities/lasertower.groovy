ea0a50f22d3d714648b9954a28ff87e4f9bb119b
ruben01 <ruben01@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-02-24 00:46:24 +0000

    added componentfromlistofclosurescomponent component this allows declaration of components without using inner clases in groovy
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@43 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

--- towerofdefense/trunk/resources/src/main/java/towerofdefense/entities/lasertower.groovy
+++ towerofdefense/trunk/resources/src/main/java/towerofdefense/entities/lasertower.groovy
@@ -1,6 +1,8 @@
 package towerofdefense.entities;
+import com.gemserk.games.towerofdefense.ComponentFromListOfClosures;
 import com.gemserk.games.towerofdefense.CountDownTimer;
 
+import com.gemserk.componentsengine.messages.GenericMessage;
 import com.gemserk.componentsengine.messages.UpdateMessage;
 
 import com.gemserk.games.towerofdefense.components.TimerComponent;
@@ -80,21 +82,39 @@ builder.entity("lasertower-${Math.random()}") {
 	property("canFireTimer",new CountDownTimer(5000))
 	property("fireDurationTimer",new CountDownTimer(2000))
 	
-	component(new ReflectionComponent("shooter"){
+	//	component(new ReflectionComponent("shooter"){
+	//				
+	//				def handleMessage(UpdateMessage message){
+	//					if(!entity.canFire || !entity.targetEntity )
+	//						return
+	//					
+	//					println "StartFire"
+	//						
+	//					entity.children[(bulletId)].enabled = true
+	//					entity.canFireTimer.reset();
+	//					entity.fireDurationTimer.reset();
+	//					entity.canFire = false
+	//					
+	//				}
+	//			})
+	
+	component(new ComponentFromListOfClosures("shooter",[
+			
+			{UpdateMessage message ->
+				if(!entity.canFire || !entity.targetEntity )
+					return
 				
-				def handleMessage(UpdateMessage message){
-					if(!entity.canFire || !entity.targetEntity )
-						return
-					
-					println "StartFire"
-						
-					entity.children[(bulletId)].enabled = true
-					entity.canFireTimer.reset();
-					entity.fireDurationTimer.reset();
-					entity.canFire = false
-					
-				}
-			})
+				println "StartFire"
+				
+				entity.children[(bulletId)].enabled = true
+				entity.canFireTimer.reset();
+				entity.fireDurationTimer.reset();
+				entity.canFire = false
+				
+			}
+			]))
+	
+	
 	
 	component(new TimerComponent("canFireTimerComponent")){
 		property("messageBuilder",utils.custom.messageBuilderFactory.messageBuilder("enableFire") { })
