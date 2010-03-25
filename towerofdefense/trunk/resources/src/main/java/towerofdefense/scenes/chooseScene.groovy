5938bfbabbc7c730f1a794d021dd6297cb680e06
acoppes <acoppes@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-03-25 16:56:26 +0000

    added paging to chooseScene
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@160 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

--- towerofdefense/trunk/resources/src/main/java/towerofdefense/scenes/chooseScene.groovy
+++ towerofdefense/trunk/resources/src/main/java/towerofdefense/scenes/chooseScene.groovy
@@ -1,8 +1,10 @@
 package towerofdefense.scenes;
 
 
-
 import com.gemserk.componentsengine.gamestates.GemserkGameState 
+import com.gemserk.componentsengine.utils.Paging 
+import com.gemserk.games.towerofdefense.components.ChildsDisablerComponent;
+
 import org.newdawn.slick.state.StateBasedGame 
 import org.newdawn.slick.state.transition.FadeInTransition 
 import org.newdawn.slick.state.transition.FadeOutTransition 
@@ -23,7 +25,7 @@ builder.entity("sceneselection") {
 			[desc:"Eighth scene",script:"towerofdefense.scenes.scene8"],
 			[desc:"Ninth scene",script:"towerofdefense.scenes.scene9"],
 			]
-			
+	
 	if(utils.custom.gameStateManager.gameProperties.runningFromMain)
 		scenes << [desc:"Test scene",script:"towerofdefense.scenes.testScene"]
 	
@@ -36,24 +38,93 @@ builder.entity("sceneselection") {
 	
 	def menuX = 400
 	def menuY = 50
-	def buttonHeight = 40
+	def buttonHeight = 60
 	def buttonSeparation = 15
 	
-	scenes.eachWithIndex { scene, i ->
-		child(template:"towerofdefense.entities.button", id:"buttonScene-$i".toString())	{
-			position=utils.vector(menuX, (Float)(menuY + i*(buttonHeight+buttonSeparation)))
-			rectangle=utils.rectangle(-160, (float)(-buttonHeight/2f), 320, buttonHeight)
-			//label={entity.parent.currentPage[itemNumber]}
-			label=scene.desc
-			lineColor=utils.color(0f, 0f, 1f, 0.5f)
-			mouseNotOverFillColor=buttonMouseOverFillColor
-			mouseOverFillColor=buttonMouseNotOverFillColor
-			font=buttonFont
-			trigger=utils.custom.triggers.genericMessage("loadScene") {
-				message.scene = scene.script
+	def itemsPerPage = 6
+	
+	def paging = new Paging(scenes, itemsPerPage)
+	
+	property("paging", paging)
+	
+	itemsPerPage.times { i -> 
+	
+		println i
+		
+		child(entity("button-$i".toString()) { 
+			
+			component(new ChildsDisablerComponent("enabler")) {
+				property("enabled", {paging.onPageHasItem(i)})
 			}
-		}
+			
+			child(entity("buttonScene-$i".toString()) {
+				
+				property("scene", {paging.getItem(i)})
+				
+				parameters = [
+								position:utils.vector(menuX, (Float)(menuY + i*(buttonHeight+buttonSeparation))),
+								rectangle:utils.rectangle(-160, (float)(-buttonHeight/2f), 320, buttonHeight),
+								label:{entity.scene.desc},
+								lineColor:utils.color(0f, 0f, 1f, 0.5f),
+								mouseNotOverFillColor:buttonMouseOverFillColor,
+								mouseOverFillColor:buttonMouseNotOverFillColor,
+								font:buttonFont,
+								trigger:utils.custom.triggers.genericMessage("loadScene") {
+									message.scene = entity.scene.script
+								}
+				  		]
+				  		
+				parent("towerofdefense.entities.button", parameters)
+				
+			})
+			
+		})
+	
 	}
+	
+	child(entity("nextPageButton") {
+		
+		property("paging", {entity.parent.paging})
+		
+		parameters = [
+			position:utils.vector(680, 550),
+			rectangle:utils.rectangle(-100, (float)(-buttonHeight/2f), 200, buttonHeight),
+			label:"next",
+			lineColor:utils.color(0f, 0f, 1f, 0.5f),
+			mouseNotOverFillColor:buttonMouseOverFillColor,
+			mouseOverFillColor:buttonMouseNotOverFillColor,
+			font:buttonFont,
+			trigger:utils.custom.triggers.genericMessage("nextPage") {
+				entity.paging.nextPage()
+			},
+			enabled:{!entity.paging.isLastPage()}
+		]
+		
+		parent("towerofdefense.entities.button", parameters)
+		
+	})
+
+	child(entity("previousPageButton") {
+
+		property("paging", {entity.parent.paging})
+		
+		parameters = [
+			position:utils.vector(120, 550),
+			rectangle:utils.rectangle(-100, (float)(-buttonHeight/2f), 200, buttonHeight),
+			label:"previous",
+			lineColor:utils.color(0f, 0f, 1f, 0.5f),
+			mouseNotOverFillColor:buttonMouseOverFillColor,
+			mouseOverFillColor:buttonMouseNotOverFillColor,
+			font:buttonFont,
+			trigger:utils.custom.triggers.genericMessage("previousPage") {
+				entity.paging.previousPage()
+			},
+			enabled:{!entity.paging.isFirstPage()}
+		]
+		
+		parent("towerofdefense.entities.button", parameters)
+		
+	})
 	
 	component(utils.components.genericComponent(id:"loadSceneHandler", messageId:"loadScene"){ message ->
 		StateBasedGame stateBasedGame = utils.custom.gameStateManager
@@ -67,7 +138,6 @@ builder.entity("sceneselection") {
 		StateBasedGame stateBasedGame = utils.custom.gameStateManager
 		stateBasedGame.enterState(0, new FadeOutTransition(), new FadeInTransition())
 	})
-	
 	
 	input("inputmapping"){
 		keyboard {
