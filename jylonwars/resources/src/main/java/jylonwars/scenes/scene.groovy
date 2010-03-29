206d83e0788b0f0f24f9f130931d41b4ffac2f5c
ruben01 <ruben01@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-03-29 19:39:44 +0000

    initial import
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@171 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

---
+++ jylonwars/resources/src/main/java/jylonwars/scenes/scene.groovy
@@ -0,0 +1,13 @@
+package jylonwars.scenes;
+
+import com.gemserk.componentsengine.gamestates.GemserkGameState 
+import org.newdawn.slick.state.StateBasedGame 
+import org.newdawn.slick.state.transition.FadeInTransition 
+import org.newdawn.slick.state.transition.FadeOutTransition 
+import jylonwars.GroovyBootstrapper 
+
+
+builder.entity("scene") {
+	
+	new GroovyBootstrapper();
+}
