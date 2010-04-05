b2745a741a40b654fde8f3321a74b6e7849d6ebb
ruben01 <ruben01@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-04-05 21:40:50 +0000

    This is a test to check if empty project breaks maven webstar plugin
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@181 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

---
+++ jylonwars/trunk/game/src/main/java/com/gemserk/games/jylonwars/TestMain.java
@@ -0,0 +1,7 @@
+package com.gemserk.games.jylonwars;
+
+public class TestMain {
+	public static void main(String[] args) {
+		System.out.println("This is a test to check if empty project breaks maven webstar plugin");
+	}
+}
