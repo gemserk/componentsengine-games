4d04b9f3ffe74b48972b9e84c67fa987cd79b4e1
acoppes <acoppes@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-06-08 19:21:33 +0000

    changed cursor
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@309 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

--- zombierockers/trunk/resources/src/main/java/zombierockers/scenes/playing.groovy
+++ zombierockers/trunk/resources/src/main/java/zombierockers/scenes/playing.groovy
@@ -13,6 +13,8 @@ builder.entity {
 	
 	child(entity("cursor"){
 		
+		property("color", {entity.parent.children["world"].children["cannon"].currentBall.color})
+		
 		property("position",utils.vector(400,300))
 		property("bounds",utils.rectangle(20,20,760,520))
 		
@@ -21,14 +23,13 @@ builder.entity {
 			propertyRef("position","position")
 		}
 		
-		
 		component(utils.components.genericComponent(id:"mousemovehandler", messageId:["movemouse"]){ message ->
 			entity.position =  utils.vector(message.x,message.y)
 		})
 		
 		component(new ImageRenderableComponent("imagerenderer")) {
-			property("image", utils.resources.image("ship"))
-			property("color", utils.color(1,1,1,1))
+			property("image", utils.resources.image("cursor"))
+			propertyRef("color", "color")
 			property("position", {entity.position})
 			property("direction", utils.vector(0,1))
 		}
@@ -50,37 +51,37 @@ builder.entity {
 		}
 	}
 	
-//	TEMPORAL PATH EDITOR
-//	property("newPositions", [])
-//	
-//	property("path", new Path([]))
-//	
-//	child(entity("editor.newPathRenderer"){ 
-//
-//		component(new PathRendererComponent("editor.pathRendererFill")){
-//			property("lineColor", utils.color(0.5f, 0.5f, 1f, 1.0f))
-//			property("lineWidth", 10.0f)
-//			property("path", {entity.parent.path})		
-//		}
-//		
-//	})
-//	
-//	
-//	component(utils.components.genericComponent(id:"editor.putPositionHandler", messageId:"rightmouse"){ message ->
-//		entity.newPositions << entity.mousePosition
-//		log.info("Editor new position : $entity.mousePosition")
-//		
-//		entity.path = new Path(entity.newPositions)
-//	})
-//	
-//	component(utils.components.genericComponent(id:"editor.mouseMoveHandler", messageId:"movemouse"){ message ->
-//		entity.mousePosition = new Vector2f(message.x, message.y)
-//	})
-//	
-//	component(utils.components.genericComponent(id:"editor.dumpPositions", messageId:"dumpEditorPositions"){ message ->
-//		log.info("newPositions dump ")
-//		entity.newPositions.each {   println "utils.vector(${it.x}f, ${it.y}f)," }
-//	})
+	//	TEMPORAL PATH EDITOR
+	//	property("newPositions", [])
+	//	
+	//	property("path", new Path([]))
+	//	
+	//	child(entity("editor.newPathRenderer"){ 
+	//
+	//		component(new PathRendererComponent("editor.pathRendererFill")){
+	//			property("lineColor", utils.color(0.5f, 0.5f, 1f, 1.0f))
+	//			property("lineWidth", 10.0f)
+	//			property("path", {entity.parent.path})		
+	//		}
+	//		
+	//	})
+	//	
+	//	
+	//	component(utils.components.genericComponent(id:"editor.putPositionHandler", messageId:"rightmouse"){ message ->
+	//		entity.newPositions << entity.mousePosition
+	//		log.info("Editor new position : $entity.mousePosition")
+	//		
+	//		entity.path = new Path(entity.newPositions)
+	//	})
+	//	
+	//	component(utils.components.genericComponent(id:"editor.mouseMoveHandler", messageId:"movemouse"){ message ->
+	//		entity.mousePosition = new Vector2f(message.x, message.y)
+	//	})
+	//	
+	//	component(utils.components.genericComponent(id:"editor.dumpPositions", messageId:"dumpEditorPositions"){ message ->
+	//		log.info("newPositions dump ")
+	//		entity.newPositions.each {   println "utils.vector(${it.x}f, ${it.y}f)," }
+	//	})
 	
 	component(utils.components.genericComponent(id:"pauseGameHandler", messageId:"pauseGame"){ message ->
 		if (messageQueue.messages.isEmpty())
