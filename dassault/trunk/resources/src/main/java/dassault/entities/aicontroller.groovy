fe740c41396ec3e1e5326b4d8babef087238b8f7
acoppes <acoppes@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-07-03 12:49:52 +0000

    added simple wander behavior to the ai controlled droids
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@357 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

--- dassault/trunk/resources/src/main/java/dassault/entities/aicontroller.groovy
+++ dassault/trunk/resources/src/main/java/dassault/entities/aicontroller.groovy
@@ -12,12 +12,29 @@ builder.entity {
 	component(utils.components.genericComponent(id:"controllerComponent", messageId:"update"){ message ->
 		
 		def player = entity.player
+		def delta = message.delta
 		
 		def controlledEntities = entity.root.getEntities(Predicates.and(EntityPredicates.withAllTags(player, "droid")))
 		
 		controlledEntities.each { controlledEntity -> 
 			
-			controlledEntity.moveDirection = utils.vector(1,0)
+			def wanderDirection = controlledEntity.wanderDirection ?: utils.vector(1,0)
+			def wanderTime = controlledEntity.wanderTime ?: 0
+					
+			if (wanderTime <= 0) {
+				wanderDirection.x = (float) utils.random.nextFloat() - 0.5f
+				wanderDirection.y = (float) utils.random.nextFloat() - 0.5f
+				wanderDirection.normalise()
+				
+				wanderTime = utils.random.nextInt(1000) + 1000
+				println "wanderDirection : $wanderDirection , wanderTime : $wanderTime"
+			} else {
+				wanderTime = wanderTime - delta
+			}
+			
+			controlledEntity.wanderDirection = wanderDirection.copy()
+			controlledEntity.moveDirection = controlledEntity.wanderDirection.copy()
+			controlledEntity.wanderTime = wanderTime
 			
 		}
 	})
