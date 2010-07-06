73fb7ce26eb18f625ce58be9c27464016c25dcdd
acoppes <acoppes@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-07-06 22:13:20 +0000

    fixed camera should be between selected droid and mouse position. TODO: fix that when zoomed view
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@380 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

--- dassault/trunk/resources/src/main/java/dassault/entities/camera.groovy
+++ dassault/trunk/resources/src/main/java/dassault/entities/camera.groovy
@@ -16,37 +16,55 @@ builder.entity {
 	component(utils.components.genericComponent(id:"updateCameraPosition", messageId:["update"]){ message ->
 		
 		def screen = entity.screen
+		
+		// center screen, could be considered as start camera position
 		def centerPosition = utils.vector((float)screen.width * 0.5f, (float)screen.height * 0.5f)
 		
 		def player = entity.root.getEntityById(entity.ownerId)
 		def droid = entity.root.getEntityById(player.controlledDroidId)
 		
-		if (!droid)
-		{
+		if (!droid) {
 			// lost control of main droid
 			entity.position = utils.vector(0,0)
 			return
 		}
 		
-		def targetPosition = droid.position.copy().sub(centerPosition).negate()
+		def input = utils.custom.gameContainer.input
 		
+		//// 
+		
+		def targetPosition = droid.position.copy().sub(centerPosition).negate()
 		entity.position =  targetPosition
 		
 		// if followMouse then mid point between mouse and owner position
-		def input = utils.custom.gameContainer.input
 		
+		// current position of the mouse, based on current camera
 		def mousePosition = utils.vector((float) input.getMouseX(), (float) input.getMouseY())
-		entity.mouseRelativePosition = mousePosition.sub(centerPosition)
+		entity.mouseRelativePosition = mousePosition.copy().sub(centerPosition)
 		
 		if (!entity.followMouse)
 			return
 		
-		mousePosition.negateLocal();
+		def oldCenterPosition = utils.vector((float)screen.width * 0.5f, (float)screen.height * 0.5f)
 		
-		targetPosition = targetPosition.add(mousePosition).scale(0.5f)
+		def mousePositionRelativoAlNuevoZero = utils.vector((float) input.getMouseX(), (float) input.getMouseY())
+		
+		def transformation = entity.position
+		def mouseAbsolutePosition = mousePositionRelativoAlNuevoZero.copy().sub(transformation)
+		
+//		println "oldCenter = $oldCenterPosition"
+//		println "mousePositionRelativoAlNuevoZero = $mousePositionRelativoAlNuevoZero"
+//		println "transformation = $transformation"
+//		println "mouseAbsolutePosition = $mouseAbsolutePosition"
+		
+		mouseAbsolutePosition.sub(centerPosition)
+		mouseAbsolutePosition.negateLocal()
+		
+		targetPosition = targetPosition.add(mouseAbsolutePosition).scale(0.5f)
+		
+//		println "targetPosition = $targetPosition"
+		
 		entity.position = targetPosition
-		
-		//		entity.position = mousePosition
 	})
 	
 	component(utils.components.genericComponent(id:"toggleFollow", messageId:["toggleFollowMouse"]){ message ->
@@ -82,7 +100,7 @@ builder.entity {
 	
 	input("inputmapping"){
 		keyboard {
-			//			press(button:"e",eventId:"toggleFollowMouse")
+			press(button:"e",eventId:"toggleFollowMouse")
 			press(button:"z",eventId:"toggleZoom")
 		}
 		mouse {
