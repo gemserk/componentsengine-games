c7fab795b2606e54fd1e8f201aa2abdfd445c4bc
acoppes <acoppes@1dcdb8b7-3cac-4752-9c2b-54fd448f9028>
2010-07-16 02:57:34 +0000

    changed the limits to be autocalculated based on the lower and upper bounds.
    git-svn-id: file:///d/Documents/workspace/gemserksvn/var/svn/games@415 1dcdb8b7-3cac-4752-9c2b-54fd448f9028

--- dassault/trunk/resources/src/main/java/dassault/scenes/playing.groovy
+++ dassault/trunk/resources/src/main/java/dassault/scenes/playing.groovy
@@ -17,7 +17,7 @@ builder.entity("playing") {
 	new GroovyBootstrapper();
 	
 	child(id:"light1", template:"dassault.entities.pointlight") { 
-		position = utils.vector(100,100)
+		position = utils.vector(-900,-900)
 		layer = 10
 		size = 5f
 		time = 1500
@@ -26,7 +26,7 @@ builder.entity("playing") {
 	}
 	
 	child(id:"light2", template:"dassault.entities.pointlight") { 
-		position = utils.vector(700,100)
+		position = utils.vector(900,-900)
 		layer = 10
 		size = 5f
 		time = 750
@@ -35,7 +35,7 @@ builder.entity("playing") {
 	}
 	
 	child(id:"light3", template:"dassault.entities.pointlight") { 
-		position = utils.vector(100,500)
+		position = utils.vector(-900,900)
 		layer = 10
 		size = 8f
 		time = 1000
@@ -46,7 +46,7 @@ builder.entity("playing") {
 	child(entity("light4") {
 		
 		parent("dassault.entities.pointlight", [
-		position: utils.vector(700,500),
+		position: utils.vector(0, 0),
 		layer: 10,
 		size: 8f,
 		time: 3000,
@@ -129,12 +129,12 @@ builder.entity("playing") {
 		component(utils.components.genericComponent(id:"gameOverHandler", messageId:"gameover"){ message ->
 			utils.custom.messageQueue.enqueue(utils.genericMessage("zoom"){ newMessage ->
 				newMessage.cameraId = "camera"
-				newMessage.end = 0.5f
+				newMessage.end = 0.3f
 				newMessage.time = 500
 			})	
 			utils.custom.messageQueue.enqueue(utils.genericMessage("moveTo"){ newMessage ->
 				newMessage.entityId = "camera"
-				newMessage.target = utils.vector(400, 300)
+				newMessage.target = utils.vector(0, 0)
 				newMessage.time = 300
 			})
 			
@@ -252,66 +252,70 @@ builder.entity("playing") {
 	
 	// def obstacleForm3 = new Polygon([-13.18f, -19.1f, 16.82f, -18.87f, 45f, -26f, 101f, -66f, 169f, -75f, 175f, -12f, 77f, -17f, 47f, -3f, 16.58f, 11.13f, 38f, 18f, 135f, 65f, 52f, 41f, 33f, 57f, 29f, 32f, 7f, 41f, 1.5f, 21.01f, -13.42f, 10.89f, -8.29f, -5.07f] as float[])
 	def obstacleForm3 = new Polygon([58.05f, 46.16f, 30.43f, 34.46f, 0.74f, 29.88f, -19.3f, 12.92f, -45.41f, 36.74f, -36.27f, 64.34f, -23.31f, 82.7f, -5.43f, 63.05f, 15.03f, 60.7f, 23.15f, 80.29f, 5.15f, 79.33f, -2.35f, 100.04f, -28.48f, 110.86f, -44.37f, 89.13f, -50.19f, 63.99f, -85.88f, 66.52f, -108.5f, 63.65f, -117.48f, 38.28f, -117.38f, 14.51f, -123.29f, -18.25f, -110.82f, -50.97f, -108.96f, -81.59f, -73.41f, -73.35f, -54.89f, -84.08f, -31.15f, -96.98f, 1.19f, -83.59f, 19.89f, -79.09f, 46.05f, -76.91f, 71.44f, -72.89f, 96.07f, -67.01f, 120.48f, -57.99f, 146.91f, -32.97f, 152.51f, -4.67f, 160.25f, 15.85f, 135.51f, 33.75f, 112.19f, 27.34f, 97.92f, 9.52f, 120.68f, 1.63f, 113.48f, -17.59f, 93.45f, -34.55f, 69.74f, -40.04f, 42.89f, -45.75f, 18.1f, -53.85f, -2.77f, -63.58f, -27.49f, -77.06f, -49.23f, -61.17f, -69.37f, -54.36f, -89.82f, -52.01f, -92.38f, -24.93f, -102.56f, 2.25f, -100.43f, 25.86f, -94.61f, 51f, -65.55f, 43.56f, -63.59f, 20.55f, -59.06f, -16.54f, -9.89f, -12.41f, 7.08f, 11.94f, 24.58f, -6.79f, 42.13f, 6.83f, 59.84f, 3.47f, 69.75f, 18.53f, 58.91f, 31.31f] as float[])
-
-	// def obstacleForm =
 	
-	//  
+	// def obstacleForm =
 	
 	obstacleForm.centerX = 0f
 	obstacleForm.centerY = 0f
 	
 	obstacleForm2.centerX = 0f
 	obstacleForm2.centerY = 0f
-
+	
 	Transform rotationMatrix = Transform.createRotateTransform(30);
-
-//	child(id:"obstacle1", template:"dassault.entities.obstacle") { 
-//		position = utils.vector(200,200)
-//		// bounds = utils.rectangle(-50, -10, 100, 20)
-//		bounds = obstacleForm.transform(Transform.createScaleTransform(30f, 30f))
-//		color = utils.color(0.1f,0f,0f,1f)
-//		layer = 19
-//	}
-//	
-//	child(id:"obstacle2", template:"dassault.entities.obstacle") { 
-//		position = utils.vector(400,400)
-//		// bounds = utils.rectangle(-50, -10, 100, 20)
-//		bounds = obstacleForm2.transform(rotationMatrix).transform(Transform.createScaleTransform(2f, 2f))
-//		color = utils.color(0.1f,0f,0f,1f)
-//		layer = 19
-//	}
+	
+	child(id:"obstacle1", template:"dassault.entities.obstacle") { 
+		position = utils.vector(-700, 0)
+		// bounds = utils.rectangle(-50, -10, 100, 20)
+		bounds = obstacleForm.transform(Transform.createScaleTransform(40f, 40f))
+		color = utils.color(0.0f,0f,0f,1f)
+		layer = 19
+	}
+	
+	//	
+	//	child(id:"obstacle2", template:"dassault.entities.obstacle") { 
+	//		position = utils.vector(400,400)
+	//		// bounds = utils.rectangle(-50, -10, 100, 20)
+	//		bounds = obstacleForm2.transform(rotationMatrix).transform(Transform.createScaleTransform(2f, 2f))
+	//		color = utils.color(0.1f,0f,0f,1f)
+	//		layer = 19
+	//	}
 	
 	child(id:"obstacleForm3", template:"dassault.entities.obstacle") { 
-		position = utils.vector(200,100)
-		// bounds = utils.rectangle(-50, -10, 100, 20)
-		bounds = obstacleForm3.transform(Transform.createScaleTransform(3f, 3f)).transform(Transform.createRotateTransform(1f))
-		color = utils.color(0.1f,0.1f,0f,1f)
+		position = utils.vector(200,-100)
+		bounds = obstacleForm3.transform(Transform.createScaleTransform(5f, 5f)).transform(Transform.createRotateTransform(1f))
+		color = utils.color(0.0f,0.0f,0f,1f)
 		layer = 19
 	}
 	
 	// scene limits as obstacles
 	
+	def lowerBound = utils.vector(-1000, -1000)
+	def upperBound = utils.vector(1000, 1000)
+	
+	def xmidpoint = (float)((lowerBound.x + upperBound.x) / 2f)
+	def ymidpoint = (float)((lowerBound.y + upperBound.y) / 2f)
+	
 	child(id:"sceneLimit1", template:"dassault.entities.obstacle") { 
-		position = utils.vector(-1000,300)
-		bounds = utils.rectangle(-1010, -2000, 2020, 4000)
+		position = utils.vector(lowerBound.x, ymidpoint)
+		bounds = utils.rectangle(-20, 0, 40, (float)(upperBound.y - lowerBound.y))
 		color = utils.color(0,0,0,1)
 	}
 	
 	child(id:"sceneLimit2", template:"dassault.entities.obstacle") { 
-		position = utils.vector(1800,300)
-		bounds = utils.rectangle(-1010, -2000, 2020, 4000)
+		position = utils.vector(upperBound.x, ymidpoint)
+		bounds = utils.rectangle(-20, 0, 40, (float)(upperBound.y - lowerBound.y))
 		color = utils.color(0,0,0,1)
 	}
 	
 	child(id:"sceneLimit3", template:"dassault.entities.obstacle") { 
-		position = utils.vector(400,-1000)
-		bounds = utils.rectangle(-400, -1010, 800, 2020)
+		position = utils.vector(xmidpoint, lowerBound.y)
+		bounds = utils.rectangle(0, -20,(float)(upperBound.x - lowerBound.x), 40)
 		color = utils.color(0,0,0,1)
 	}
 	
 	child(id:"sceneLimit4", template:"dassault.entities.obstacle") { 
-		position = utils.vector(400,1600)
-		bounds = utils.rectangle(-400, -1010, 800, 2020)
+		position = utils.vector(xmidpoint, upperBound.y)
+		bounds = utils.rectangle(0, -20, (float)(upperBound.x - lowerBound.x), 40)
 		color = utils.color(0,0,0,1)
 	}
 	
@@ -360,7 +364,7 @@ builder.entity("playing") {
 	def globalWeaponTypes = globalWeaponFactory.keySet().collect { it }
 	
 	child(id:"spawner1", template:"dassault.entities.droidspawner") { 
-		position = utils.vector(100,100)
+		position = utils.vector(-900,-900)
 		minTime = 5000
 		maxTime = 5000
 		droidFactory = globalDroidFactory
@@ -371,7 +375,7 @@ builder.entity("playing") {
 	}
 	
 	child(id:"spawner2", template:"dassault.entities.droidspawner") { 
-		position = utils.vector(700,100)
+		position = utils.vector(900,-900)
 		minTime = 5000
 		maxTime = 5000
 		droidFactory = globalDroidFactory
@@ -382,7 +386,7 @@ builder.entity("playing") {
 	}
 	
 	child(id:"spawner3", template:"dassault.entities.droidspawner") { 
-		position = utils.vector(100,500)
+		position = utils.vector(-900,900)
 		minTime = 5000
 		maxTime = 5000
 		droidFactory = globalDroidFactory
@@ -398,17 +402,15 @@ builder.entity("playing") {
 	
 	component(new ExplosionComponent("explosions")) { }
 	
-	property("zoomIn", true)
 	property("cameraId", "camera")
+	
+	property("zooms", [1.0f, 1.7f, 0.3f])
+	property("currentZoom", 0)
 	
 	component(utils.components.genericComponent(id:"toggleZoom", messageId:"toggleZoom"){ message ->
 		
-		def zoomTo = 1.0f
-		
-		if (entity.zoomIn) 
-			zoomTo = 1.7f
-		
-		entity.zoomIn = !entity.zoomIn
+		entity.currentZoom = (entity.currentZoom + 1) % entity.zooms.size() 
+		def zoomTo = entity.zooms[entity.currentZoom]
 		
 		def time = 200
 		utils.custom.messageQueue.enqueue(utils.genericMessage("zoom"){ newMessage ->
